b31fd42b1681b5cfb66fda55dede65cbee595e4c
muhi <muhi@livecords.com>
2020-03-16 07:24:39 -0400

    Textract new module

---
+++ index.js
@@ -0,0 +1,103 @@
+exports.createForm = (data, config) => {
+    const {
+        keyMap,
+        valueMap,
+        blockMap
+    } = getKeyValueMap(data.Blocks)
+
+    // Get Key Value relationship
+    const trimChars = config ? config.trimChars : undefined
+    return getKeyValueRelationship(keyMap, valueMap, blockMap, trimChars)
+}
+
+function getKeyValueMap(blocks) {
+    // get key and value maps
+    const keyMap = {}
+    const valueMap = {}
+    const blockMap = {}
+    try {
+        for (const block of blocks) {
+            const blockId = block.Id
+            blockMap[blockId] = block
+            if (block.BlockType === 'KEY_VALUE_SET') {
+                if (block.EntityTypes.includes('KEY')) { keyMap[blockId] = block } else { valueMap[blockId] = block }
+            }
+        }
+    } catch (err) {
+        console.log(err)
+    }
+    return {
+        keyMap,
+        valueMap,
+        blockMap
+    }
+}
+
+function getKeyValueRelationship(keyMap, valueMap, blockMap, trimChars) {
+    const kvs = {}
+    try {
+        for (const keyBlock in keyMap) {
+            const valueBlock = findValueBlock(keyMap[keyBlock], valueMap)
+            let key = getText(keyMap[keyBlock], blockMap)
+            const val = getText(valueBlock, blockMap)
+            if (key.indexOf('.') > -1) {
+                key = key.split('.').join('')
+            }
+            if (trimChars) {
+                for (let i = 0; i < trimChars.length; i++) {
+                    if (trimChars[i] === ' ') {
+                        key = key.trim()
+                    } else {
+                        key = key.replace(trimChars[i], '')
+                    }
+                }
+            }
+            kvs[key] = val
+        }
+    } catch (err) {
+        console.error(err)
+    }
+    return kvs
+}
+
+function getText(result, blocksMap) {
+    let text = ''
+    try {
+        if (result.Relationships) {
+            for (const relationship of result.Relationships) {
+                if (relationship.Type === 'CHILD') {
+                    for (const childId of relationship.Ids) {
+                        const word = blocksMap[childId]
+                        if (word.BlockType === 'WORD') {
+                            text = text + word.Text + ' '
+                        }
+                        if (word.BlockType === 'SELECTION_ELEMENT') {
+                            if (word.SelectionStatus === 'SELECTED') {
+                                text = text + 'X '
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    } catch (err) {
+        console.error(err)
+    }
+    return text
+}
+
+function findValueBlock(keyBlock, valueMap) {
+    let valueBlock = null
+    try {
+        for (const relationship of keyBlock.Relationships) {
+            if (relationship.Type === 'VALUE') {
+                for (const valueId of relationship.Ids) {
+                    valueBlock = valueMap[valueId]
+                }
+            }
+        }
+    } catch (err) {
+        console.error(err)
+    }
+    return valueBlock
+}
