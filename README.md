eb8fb149876f9a35faa8f7f84ae566897205182d
muhimasri <muhimasri@gmail.com>
2020-03-29 15:48:31 -0400

    Support analyzing tables

--- README.md
+++ README.md
@@ -1,17 +1,29 @@
-# Aws Textract Helper
-This is a JavaScript library (still in beta) aims to simplify and provide an easy to use methods that can help extracting meaningful data from Amazon Textract APIs. Currently it only supports forms (FeatureTypes: ['FORMS']) but we are working on a plan to support other features including tables and publish it as an open source project. For now, you can freely use it to extract forms.
+# AWS Textract Helper
+This is a JavaScript library that provides an easy to use methods for extracting structured data from Amazon Textract APIs.
 
-# Installing
+## Prerequisites
+You need to have a basic knowledge of AWS and Textract APIs. For a quick introduction, please refere to [this article](https://levelup.gitconnected.com/convert-a-form-image-to-an-html-form-using-amazon-textract-and-nodejs-d4d7c1a2b0c5) and [repository example](https://github.com/muhimasri/aws-textract-app) to help you getting started.
+
+## Installation
 ```
 npm i aws-textract-helper
 ```
 
-# Example
+## Documentation
+| Method        | Description   |
+| ------------- | ------------- |
+| createForm    | Analyze blocks returned by Textract and creates a key-value object that represents a form in an image |
+| createTables  | Analyze blocks returned by Textract and creates an array of tables that represents all the tables in an image |
+
+## Examples
+
+### Create a form
+![alt text](https://livecords-dev.s3.us-west-2.amazonaws.com/form-application-small.jpg-1585509821204 "Members Table")
 ```
 const textractHelper = require('aws-textract-helper')
 const form = textractHelper.createForm(dataFromTextract)
 ```
-It will analyze all blocks and return a JSON object representing the form
+It will analyze all blocks and return a JSON object representing a form
 ```
 {
     "First Name": "John",
@@ -19,8 +31,37 @@ It will analyze all blocks and return a JSON object representing the�
     "Address": "Planet Earth"
 }
 ```
+### Create tables
+![alt text](https://livecords-dev.s3.us-west-2.amazonaws.com/member-table.PNG-1585509527854 "Members Table")
+```
+const textractHelper = require('aws-textract-helper')
+const tables = textractHelper.createTables(dataFromTextract)
+```
+It will analyze all blocks and return an array of tables
+```
+[{
+	"1": {
+		"1": "Memberld",
+		"2": "First Name",
+		"3": "Last Name",
+		"4": "Address"
+	},
+	"2": {
+		"1": "111",
+		"2": "Muhi",
+		"3": "Masri",
+		"4": "Planet Earth"
+	},
+	"3": {
+		"1": "222",
+		"2": "John",
+		"3": "Smith",
+		"4": "Planet Mars"
+	}
+}]
+```
 
-# Config Option
+### Extra options
 You can use a trimChar config option to remove unwanted characters in form keys such as a colon or extra spaces
 ```
 const form = textractHelper.createForm(dataFromTextract, { trimChars: [':', ' '] })
--- index.js
+++ index.js
@@ -1,103 +1,145 @@
 exports.createForm = (data, config) => {
-    const {
-        keyMap,
-        valueMap,
-        blockMap
-    } = getKeyValueMap(data.Blocks)
+  const {
+    keyMap,
+    valueMap,
+    blockMap
+  } = getKeyValueMap(data.Blocks)
 
-    // Get Key Value relationship
-    const trimChars = config ? config.trimChars : undefined
-    return getKeyValueRelationship(keyMap, valueMap, blockMap, trimChars)
+  // Get Key Value relationship
+  const trimChars = config ? config.trimChars : undefined
+  return getKeyValueRelationship(keyMap, valueMap, blockMap, trimChars)
 }
 
-function getKeyValueMap(blocks) {
-    // get key and value maps
-    const keyMap = {}
-    const valueMap = {}
-    const blockMap = {}
-    try {
-        for (const block of blocks) {
-            const blockId = block.Id
-            blockMap[blockId] = block
-            if (block.BlockType === 'KEY_VALUE_SET') {
-                if (block.EntityTypes.includes('KEY')) { keyMap[blockId] = block } else { valueMap[blockId] = block }
-            }
-        }
-    } catch (err) {
-        console.log(err)
+exports.createTables = (data) => {
+  const blocks = data.Blocks
+
+  const blocksMap = {}
+  const tableBlocks = []
+  for (const block of blocks) {
+    blocksMap[block.Id] = block
+    if (block.BlockType === 'TABLE') {
+      tableBlocks.push(block)
     }
-    return {
-        keyMap,
-        valueMap,
-        blockMap
-    }
+  }
+  const tableSets = []
+  for (const table of tableBlocks) {
+    tableSets.push(getRowsColumnsMap(table, blocksMap))
+  }
+  return tableSets
 }
 
-function getKeyValueRelationship(keyMap, valueMap, blockMap, trimChars) {
-    const kvs = {}
-    try {
-        for (const keyBlock in keyMap) {
-            const valueBlock = findValueBlock(keyMap[keyBlock], valueMap)
-            let key = getText(keyMap[keyBlock], blockMap)
-            const val = getText(valueBlock, blockMap)
-            if (key.indexOf('.') > -1) {
-                key = key.split('.').join('')
-            }
-            if (trimChars) {
-                for (let i = 0; i < trimChars.length; i++) {
-                    if (trimChars[i] === ' ') {
-                        key = key.trim()
-                    } else {
-                        key = key.replace(trimChars[i], '')
-                    }
-                }
-            }
-            kvs[key] = val
-        }
-    } catch (err) {
-        console.error(err)
+function getKeyValueMap (blocks) {
+  // get key and value maps
+  const keyMap = {}
+  const valueMap = {}
+  const blockMap = {}
+  try {
+    for (const block of blocks) {
+      const blockId = block.Id
+      blockMap[blockId] = block
+      if (block.BlockType === 'KEY_VALUE_SET') {
+        if (block.EntityTypes.includes('KEY')) { keyMap[blockId] = block } else { valueMap[blockId] = block }
+      }
     }
-    return kvs
+  } catch (err) {
+    console.log(err)
+  }
+  return {
+    keyMap,
+    valueMap,
+    blockMap
+  }
 }
 
-function getText(result, blocksMap) {
-    let text = ''
-    try {
-        if (result.Relationships) {
-            for (const relationship of result.Relationships) {
-                if (relationship.Type === 'CHILD') {
-                    for (const childId of relationship.Ids) {
-                        const word = blocksMap[childId]
-                        if (word.BlockType === 'WORD') {
-                            text = text + word.Text + ' '
-                        }
-                        if (word.BlockType === 'SELECTION_ELEMENT') {
-                            if (word.SelectionStatus === 'SELECTED') {
-                                text = text + 'X '
-                            }
-                        }
-                    }
-                }
-            }
+function getKeyValueRelationship (keyMap, valueMap, blockMap, trimChars) {
+  const kvs = {}
+  try {
+    for (const keyBlock in keyMap) {
+      const valueBlock = findValueBlock(keyMap[keyBlock], valueMap)
+      let key = getText(keyMap[keyBlock], blockMap)
+      const val = getText(valueBlock, blockMap)
+      if (key.indexOf('.') > -1) {
+        key = key.split('.').join('')
+      }
+      if (trimChars) {
+        for (let i = 0; i < trimChars.length; i++) {
+          if (trimChars[i] === ' ') {
+            key = key.trim()
+          } else {
+            key = key.replace(trimChars[i], '')
+          }
         }
-    } catch (err) {
-        console.error(err)
+      }
+      kvs[key] = val
     }
-    return text
+  } catch (err) {
+    console.error(err)
+  }
+  return kvs
 }
 
-function findValueBlock(keyBlock, valueMap) {
-    let valueBlock = null
-    try {
-        for (const relationship of keyBlock.Relationships) {
-            if (relationship.Type === 'VALUE') {
-                for (const valueId of relationship.Ids) {
-                    valueBlock = valueMap[valueId]
-                }
+function getText (result, blocksMap) {
+  let text = ''
+  try {
+    if (result.Relationships) {
+      for (const relationship of result.Relationships) {
+        if (relationship.Type === 'CHILD') {
+          for (const childId of relationship.Ids) {
+            const word = blocksMap[childId]
+            if (word.BlockType === 'WORD') {
+              text = text + word.Text + ' '
             }
+            if (word.BlockType === 'SELECTION_ELEMENT') {
+              if (word.SelectionStatus === 'SELECTED') {
+                text = text + 'X '
+              }
+            }
+          }
         }
-    } catch (err) {
-        console.error(err)
+      }
     }
-    return valueBlock
+  } catch (err) {
+    console.error(err)
+  }
+  return text
+}
+
+function findValueBlock (keyBlock, valueMap) {
+  let valueBlock = null
+  try {
+    for (const relationship of keyBlock.Relationships) {
+      if (relationship.Type === 'VALUE') {
+        for (const valueId of relationship.Ids) {
+          valueBlock = valueMap[valueId]
+        }
+      }
+    }
+  } catch (err) {
+    console.error(err)
+  }
+  return valueBlock
+}
+
+function getRowsColumnsMap (tableResult, blocksMap) {
+  const rows = {}
+  try {
+    for (const relationship of tableResult.Relationships) {
+      if (relationship.Type === 'CHILD') {
+        for (const childId of relationship.Ids) {
+          const cell = blocksMap[childId]
+          if (cell.BlockType === 'CELL') {
+            const rowIndex = cell.RowIndex
+            const colIndex = cell.ColumnIndex
+            if (!rows[rowIndex]) {
+              rows[rowIndex] = {}
+            }
+            rows[rowIndex][colIndex] = getText(cell, blocksMap)
+          }
+        }
+      }
+    }
+  } catch (err) {
+    console.error(err)
+  }
+  return rows
 }
